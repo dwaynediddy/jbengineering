563bbed999acffc767f3620b845d1321f62b6873
dwaynediddy <burrows.dwayne@icloud.com>
2021-12-22 12:40:52 +0100

    added state to form

--- pages/Form.js
+++ pages/Form.js
@@ -1,40 +1,79 @@
-const submitHandler = () => {
-    alert('submit successful')
-}
+import react, { useState } from 'react';
+
 
 export default function Form() {
+    // state handler for single input
+    // const [fname, setFname] = useState('')
+
+    const submitHandler = e => {
+        e.preventDefault()
+        alert('submit successful')
+    }
+
+    // const handleChange = e => {
+    //     setFname(e.target.value)
+    //     console.log(input)
+    // }
+
+    const [input, setInput] = useState({
+        fname: '',
+        lname: '',
+        email: '',
+        phoneNumber: ''
+    })
+
+    const handleChange = e => {
+        setInput({
+            ...input,
+            [e.target.name]: e.target.value,
+        })
+        console.log(input)
+    }
+
     return (
-        <div className="container">
+        <div className="formContainer">
             <h2>Contact Us</h2>
             <form className="contactForm" onSubmit={submitHandler}>
                 <label>Add Fist name</label>
                 <input 
+                    onChange={handleChange}
                     type="text"
                     placeholder="Name"
                     className='formFirstName'
+                    name='fname'
+                    value={input.fname}
                     required
                     />
                 <label>Add Last name</label>
                 <input 
-                    type="text"
-                    placeholder="Last Name"
-                    className='formLastName'
-                    />
+                onChange={handleChange}
+                type="text"
+                placeholder="Last Name"
+                className='formLastName'
+                name='lname'
+                value={input.lname}
+                />
                 <label>Add Email</label>
                 <input 
-                    type="email"
-                    placeholder="Add Email"
-                    className='formEmail'
-                    required
-                    />
+                onChange={handleChange}
+                type="email"
+                placeholder="Add Email"
+                className='formEmail'
+                name='email'
+                value={input.email}
+                required
+                />
                 <label>Add Contact Number</label>
                 <input 
+                onChange={handleChange}
                     type="number"
                     placeholder="Add contact number"
                     className='formNumber'
+                    name='phoneNumber'
+                    value={input.phoneNumber}
                     required
                     />
-                    <label>tell us whatchu want</label>
+                    <label>tell milk whatchu want</label>
                     <textarea 
                     name='message' 
                     className='formMessageArea'
---
+++ pages/component/About.js
@@ -0,0 +1,8 @@
+export default function About() {
+    return (
+        <div className="aboutContainer">
+            <h3>about section</h3>
+            <p></p>
+        </div>
+    )
+}
